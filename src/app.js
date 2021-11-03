4281a61a4b1186c60bf36b8d02b54432916be9e4
jhun0601 <ngjhunnen@gmail.com>
2021-11-03 17:22:16 +0800

    setup production webpack build

--- src/app.js
+++ src/app.js
@@ -14,20 +14,9 @@ import "./styles/styles.scss";
 import "react-dates/lib/css/_datepicker.css";
 const store = configureStore();
 
-store.dispatch(addExpense({ description: "Water Bill", amount: "2000" }));
-store.dispatch(
-  addExpense({ description: "Gas Bill", amount: "900", createdAt: 1000 })
-);
-store.dispatch(addExpense({ description: "Electric Bill", amount: "1500" }));
-
-const state = store.getState();
-const visibleExpense = getVisibleExpense(state.expenses, state.filters);
-console.log(visibleExpense);
-
 const jsx = (
-  // <AppRouter />
-  <Provider store={store}>
-    <AppRouter />
-  </Provider>
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
 );
 ReactDOM.render(jsx, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,60 @@
 // entry point -> output
 const path = require("path");
-module.exports = {
-  entry: "./src/app.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$|jsx/,
-        exclude: /node_modules/,
-        options: "babelrc",
-      },
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  externals: {
-    react: "React",
-  },
-  devtool: "eval-cheap-module-source-map",
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "public"),
-    },
-    compress: true,
-    port: 9000,
-    historyApiFallback: true,
-  },
-  performance: {
-    hints: false,
-  },
-  mode: "development",
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = (env) => {
+    const isProduction = env === "production";
+    return {
+        entry: "./src/app.js",
+        output: {
+            path: path.join(__dirname, "public"),
+            filename: "bundle.js",
+        },
+        module: {
+            rules: [
+                {
+                    loader: "babel-loader",
+                    test: /\.js$|jsx/,
+                    exclude: /node_modules/,
+                    options: "babelrc",
+                },
+                {
+                    test: /\.s?css$/,
+                    // use: ["style-loader", "css-loader", "sass-loader"],
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: "css-loader",
+                            options: { sourceMap: true },
+                        },
+                        {
+                            loader: "sass-loader",
+                            options: { sourceMap: true },
+                        },
+                    ],
+                },
+            ],
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "styles.css",
+            }),
+        ],
+        externals: {
+            react: "React",
+        },
+        devtool: isProduction ? "source-map" : "inline-source-map",
+        devServer: {
+            static: {
+                directory: path.join(__dirname, "public"),
+            },
+            compress: true,
+            port: 9000,
+            historyApiFallback: true,
+        },
+        performance: {
+            hints: false,
+        },
+        mode: isProduction ? "production" : "development",
+    };
 };
